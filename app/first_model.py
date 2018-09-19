d826cab947c486d7093f6e12d5010e068956b893
NilSagor <masum1201@gmail.com>
2018-09-19 18:17:15 +0800

    summary first

--- app/first_model.py
+++ app/first_model.py
@@ -19,3 +19,14 @@ print(knn.fit(X_train, y_train))
 X_new = np.array([[5, 2.9, 1, 0.2]])
 print('X_new.shape: {}'.format(X_new.shape))
 
+prediction = knn.predict(X_new)
+print('Prediction: {}'.format(prediction))
+print('Prediction target: {}'.format(
+	iris_dataset['target_names'][prediction]))
+
+y_pred = knn.predict(X_test)
+print('Test set Predicted:\n {}'.format(
+	y_pred))
+print('Test set score: {:.2f}'.format(np.mean(y_pred == y_test)))
+print('Test set score: {:.2f}'.format(knn.score(X_test, y_test)))
+
